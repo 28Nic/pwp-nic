c796a15b1380203beaa47cbcf2e554a669a61461
Nic Grundhoffer <ngrundhoffer@cnm.edu>
2023-10-14 17:57:06 -0600

    let him cook

--- src/script.js
+++ src/script.js
@@ -1,9 +1,9 @@
 // core version + navigation, pagination modules:
-import Swiper from 'swiper';
-import { Navigation} from 'swiper/modules';
+import Swiper from 'node_modules/swiper';
+import { Navigation} from 'node_modules/swiper/modules';
 // import Swiper and modules styles
-import 'swiper/css';
-import 'swiper/css/navigation';
+import 'node_modules/swiper/css';
+import 'node_modules/swiper/css/navigation';
 
 // init Swiper:
 const swiper = new Swiper('.swiper', {
