a539196a4e6ad5e1a8833994d0dff82d13c3786c
Nic Grundhoffer <ngrundhoffer@cnm.edu>
2023-10-15 20:50:53 -0600

    we don't need no stinkin javascript library

--- changes-and-notes.md
+++ changes-and-notes.md
@@ -1,27 +1,21 @@
 ## To be Added
+- Nav bar links
 - Banner image
 - About Me Section 
   - Image
-  - Text
-    - Copy for text
-- Lessons
-  - Javascript functionality
+  - Finalize copy
 - Projects
   - Images
-    - Photos to be taken this weekend
+    - Photos to be taken this weekend/week
   - Hover
     - Copy for hover
 
 ## Fixes
--  Hamburger icon to right on nav bar
-- Background opacity only
--  --> Just edit an image to have some opacity instead (per Marty)
-- Submit button styling
-- Resize Images for Projects
 - Resize footer images
-- h2 in banner, use flex to control horizontal overflow
 
 ## Working On Now
-- Images and copy
+- hover copy
+- all images
+- nav bar links
 
 ## Notes
--- src/script.js
+++ src/script.js
@@ -1,59 +1,60 @@
-// core version + navigation, pagination modules:
-import Swiper from '/node_modules/swiper';
-import { Navigation} from '/node_modules/swiper/modules';
-// import Swiper and modules styles
-import '/node_modules/swiper/css';
-import '/node_modules/swiper/css/navigation';
 
-// init Swiper:
-const swiper = new Swiper('.swiper', {
-    // configure Swiper to use modules
-    modules: [Navigation],
-    breakpoints: {
-       768: {
-            slidesPerView: 4,
-            spaceBetween: 0
+
+const carousel = document.querySelector("#lessons .carousel")
+const lessonsBtn = document.getElementById('lessonsBtn')
+const lessonsArray = document.querySelectorAll("#lessons .carousel-item")
+
+
+let lessonsWidth = 0
+
+// calculates length of content based on window size
+if (window.innerWidth < 768) {
+    lessonsArray.forEach((element) => {
+        lessonsWidth += element.offsetWidth
+    })
+} else {
+    lessonsArray.forEach((element, i) => {
+        if (i < lessonsArray.length - 3) {
+            lessonsWidth += element.offsetWidth
         }
-    },
-    loop: true,
-    navigation: {
-        nextEl: '.swiper-button-next'
-    },
-    slidesPerView: 1,
-    spaceBetween: 0
-});
+    })
+}
 
+// same as above but recalculates every time window is resized
+window.addEventListener("resize", (event) => {
+    lessonsWidth = 0
+    if (window.innerWidth < 768) {
+        lessonsArray.forEach((element) => {
+            lessonsWidth += element.offsetWidth
+        })
+    } else {
+        lessonsArray.forEach((element, i) => {
+            if (i < lessonsArray.length - 3) {
+                lessonsWidth += element.offsetWidth
+            }
+        })
+    }
+})
 
-
-
-
-
-// const carousel = document.querySelector("#lessons .carousel")
-// const lessonsBtn = document.getElementById('lessonsBtn')
-// const lessonsArray = document.querySelectorAll("#lessons .carousel-item")
-
-
-// let lessonsWidth = 0
-// lessonsArray.forEach((element) =>{
-//     lessonsWidth += element.offsetWidth
-// })
-//
-// let scrollDistance = 0
-// lessonsBtn.addEventListener("click", (event) => {
-//     let postItWidth
-//     if (window.innerWidth < 768) {
-//         postItWidth = window.innerWidth
-//     } else {
-//        postItWidth = window.innerWidth / 4
-//     }
-//     scrollDistance += postItWidth
-//     if (scrollDistance >= lessonsWidth) {
-//         scrollDistance = 0
-//         carousel.scrollTo(scrollDistance, 0)
-//     } else {
-//     carousel.scrollTo(scrollDistance, 0)
-//     }
-//     console.log(lessonsWidth)
-//     console.log(scrollDistance)
-// })
+// how far carousel has currently scrolled
+let scrollDistance = 0
+lessonsBtn.addEventListener("click", (event) => {
+    // determines post it width based on window size
+    let postItWidth
+    if (window.innerWidth < 768) {
+        postItWidth = window.innerWidth
+    } else {
+       postItWidth = window.innerWidth / 4
+    }
+    // sets how far carousel needs to scroll
+    scrollDistance += (postItWidth - 10)
+    // looping back to the front of carousel
+    if (scrollDistance >= lessonsWidth) {
+        scrollDistance = 0
+        carousel.scrollTo(scrollDistance, 0)
+        // moving through carousel
+    } else {
+    carousel.scrollTo(scrollDistance, 0)
+    }
+})
 
